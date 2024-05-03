821d2c17433ac46a1de9d877cffa232d72a62f39
NIRUGIRA MUHIZI Norbert <norbertmuhizi21@gmail.com>
2024-05-03 02:02:56 +0200

    fixed design

--- src/components/AdminCreateBlog.js
+++ src/components/AdminCreateBlog.js
@@ -115,7 +115,7 @@ const AdminCreateBlog = ({ articleId, onArticleChange }) => {
   return (
     <section id="new-blog">
       <div className="header2">
-	  <div className={`alert ${alert ? 'show' : 'hide'}`}>{alert}</div>
+	  <div className={`Alert ${alert ? 'show' : 'hide'}`}>{alert}</div>
         <h2 className="h2">Create Your Blog Post</h2>
       </div>
       
